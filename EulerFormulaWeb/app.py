8a2fa7a50b5df93ea8b3dd17507879087ed2613a
HeavenLy <dibyendumukherjee916@gmail.com>
2024-07-08 22:04:48 +0530

    Update app.py

--- EulerFormulaWeb/app.py
+++ EulerFormulaWeb/app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 import numpy as np
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, async_mode='threading', cors_allowed_origins="*")
 @app.route('/')
 def index():
     return render_template('index.html')
